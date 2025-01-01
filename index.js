732b61c3e8dcb94cb6c0edece8e5b6ef02853bf2
bluebrandon <blue.brandon@gmail.com>
2025-01-01 08:45:07 -0500

    port move from old project

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 // For more info see docs.battlesnake.com
 
 import runServer from './server.js';
+import { getMove } from './move.js';
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
@@ -20,10 +21,10 @@ function info() {
 
   return {
     apiversion: "1",
-    author: "",       // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
-    head: "default",  // TODO: Choose head
-    tail: "default",  // TODO: Choose tail
+    author: "bluebrandon", // TODO: Your Battlesnake Username
+    color: "#10b3cc", // TODO: Choose color
+    head: "bendr", // TODO: Choose head
+    tail: "round-bum", // TODO: Choose tail
   };
 }
 
@@ -41,55 +42,10 @@ function end(gameState) {
 // Valid moves are "up", "down", "left", or "right"
 // See https://docs.battlesnake.com/api/example-move for available data
 function move(gameState) {
+  const { you, board } = gameState;
+  const nextMove = getMove(you, board);
 
-  let isMoveSafe = {
-    up: true,
-    down: true,
-    left: true,
-    right: true
-  };
-
-  // We've included code to prevent your Battlesnake from moving backwards
-  const myHead = gameState.you.body[0];
-  const myNeck = gameState.you.body[1];
-
-  if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
-    isMoveSafe.left = false;
-
-  } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
-    isMoveSafe.right = false;
-
-  } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
-    isMoveSafe.down = false;
-
-  } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
-    isMoveSafe.up = false;
-  }
-
-  // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-  // boardWidth = gameState.board.width;
-  // boardHeight = gameState.board.height;
-
-  // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  // myBody = gameState.you.body;
-
-  // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-  // opponents = gameState.board.snakes;
-
-  // Are there any safe moves left?
-  const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
-  if (safeMoves.length == 0) {
-    console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-    return { move: "down" };
-  }
-
-  // Choose a random move from the safe moves
-  const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
-
-  // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-  // food = gameState.board.food;
-
-  console.log(`MOVE ${gameState.turn}: ${nextMove}`)
+  console.log(`MOVE ${gameState.turn}: ${nextMove}`);
   return { move: nextMove };
 }
 
---
+++ move.js
@@ -0,0 +1,272 @@
+import PF from 'pathfinding';
+
+let showDebug = true;
+const debug = (message) => showDebug && console.log(message);
+const printGrid = (grid) => {
+  debug(grid.reduce((str, row) => str + row.join(' ') + '\n', ''));
+};
+
+class DirectionManager {
+  constructor(you, board) {
+    this.you = you;
+    this.head = you.head;
+    this.board = board;
+    this.snakes = board.snakes;
+  }
+
+  getPosition(position, direction) {
+    let { x, y } = position;
+    if (direction === 'left') x -= 1;
+    if (direction === 'right') x += 1;
+    if (direction === 'up') y += 1;
+    if (direction === 'down') y -= 1;
+    return { x, y };
+  }
+
+  directionTo(position) {
+    if (position.x > this.head.x) return 'right';
+    if (position.x < this.head.x) return 'left';
+    if (position.y > this.head.y) return 'up';
+    if (position.y < this.head.y) return 'down';
+  }
+
+  getSafeMoves(position) {
+    // debug('getSafeMoves--------');
+    return ['up', 'down', 'left', 'right'].filter((direction) => {
+      const { x, y } = this.getPosition(position, direction);
+
+      // debug(direction, {x, y}, this.board.width, this.board.height);
+
+      const noOutOfBounds = x >= 0 && x < this.board.width && y >= 0 && y < this.board.height;
+
+      const noHitSnakes = this.snakes.reduce(
+        (res, snake) =>
+          res &&
+          snake.body.reduce((res, segment) => res && (segment.x !== x || segment.y !== y), true),
+        true,
+      );
+
+      // debug({ noHitSnakes, noOutOfBounds });
+
+      return noHitSnakes && noOutOfBounds;
+    });
+  }
+
+  rankDirection(direction) {
+    if (direction) {
+      const position = this.getPosition(this.head, direction);
+      return this.getSafeMoves(position).length;
+    } else {
+      return 0;
+    }
+  }
+
+  isKillerHead(position) {
+    let killerHead = false;
+    this.snakes.forEach((snake) => {
+      if (snake.body.length >= this.you.body.length && snake.id !== this.you.id) {
+        const { x, y } = snake.head;
+        if (x === position.x && y === position.y) {
+          killerHead = true;
+        }
+      }
+    });
+    return killerHead;
+  }
+
+  noAdjacentKillerHead(position) {
+    return ['up', 'down', 'left', 'right'].reduce((res, direction) => {
+      const adjacentPosition = this.getPosition(position, direction);
+      return res && !this.isKillerHead(adjacentPosition);
+    }, true);
+  }
+
+  getBestMoves() {
+    const moves = this.getSafeMoves(this.head);
+    const ranks = moves.map((move) => this.rankDirection(move));
+    const maxRank = Math.max(...ranks);
+
+    // If there are options, avoid going to spaces beside killer heads
+    if (moves.length > 1) {
+      const saferMoves = moves.filter((move) => {
+        const position = this.getPosition(this.head, move);
+        return this.noAdjacentKillerHead(position);
+      });
+      if (saferMoves.length > 0) {
+        debug('safer moves!');
+        return saferMoves;
+      }
+    }
+
+    return moves.filter((move) => this.rankDirection(move) === maxRank);
+  }
+}
+
+class PathFinding {
+  constructor(you, board) {
+    this.you = you;
+    this.head = you.head;
+    this.board = board;
+    this.snakes = board.snakes;
+    this.matrix = undefined;
+  }
+
+  getMatrix() {
+    if (this.matrix === undefined) {
+      const grid = Array(this.board.height)
+        .fill()
+        .map(() => Array(this.board.width).fill(0));
+
+      this.snakes.forEach((snake) => {
+        const isFullHealth = snake.health === 100;
+
+        snake.body.forEach((segment, index) => {
+          const { x, y } = segment;
+          const isTail = index === snake.body.length - 1;
+          const nextTailSolid = isTail && isFullHealth;
+          const isHead = index === 0;
+          const dangerousHead = isHead && snake.body.length >= this.you.body.length;
+          const isYou = snake.id === this.you.id;
+
+          if (!isTail && !isHead) {
+            grid[y][x] = 1;
+          } 
+
+          if (nextTailSolid) {
+            grid[y][x] = 1;
+          }
+
+          if (dangerousHead && !isYou) {
+            grid[y][x] = 1;
+            if (grid[y][x + 1] !== undefined) grid[y][x + 1] = 1;
+            if (grid[y][x - 1] !== undefined) grid[y][x - 1] = 1;
+            if (grid[y + 1] !== undefined) grid[y + 1][x] = 1;
+            if (grid[y - 1] !== undefined) grid[y - 1][x] = 1;
+          }
+        });
+      });
+
+      debug('matrix');
+      printGrid(grid);
+      this.matrix = new PF.Grid(grid);
+    }
+    return this.matrix;
+  }
+
+  findPathInMatrix(target, matrix) {
+    if (this.head.x === target.x && this.head.y === target.y) {
+      return [];
+    }
+    const finder = new PF.AStarFinder();
+    return finder.findPath(this.head.x, this.head.y, target.x, target.y, matrix.clone());
+  }
+
+  getShortestPath(targets) {
+    const paths = targets.map((target) => this.findPathInMatrix(target, this.getMatrix()));
+    return paths
+      .filter((path) => path.length !== 0)
+      .reduce((res, path) => (res && path.length > res.length ? res : path), null);
+  }
+
+  getNextPosition(path) {
+    return { x: path[1][0], y: path[1][1] };
+  }
+}
+
+const getMove = (you, board) => {
+  debug('--------------------------------------');
+  debug(you.name);
+  debug({ you });
+  debug({ board });
+
+  const directionManager = new DirectionManager(you, board);
+  const pathfinding = new PathFinding(you, board);
+
+  const maxBodySize = 20;
+  const smallEnough = you.body.length < maxBodySize;
+  const healthy = you.health > 40;
+  const hungry = you.health < 50;
+  const foodExists = board.food.length !== 0;
+  const tooManySnakes = board.snakes.length > 6;
+
+  // KILLING!
+  const weakSnakes = board.snakes.filter((snake) => snake.body.length < you.body.length);
+  const weakSnakesExist = weakSnakes.length !== 0;
+
+  if (weakSnakesExist && healthy && smallEnough && !tooManySnakes) {
+    const weakHeads = weakSnakes.map((snake) => snake.head);
+    const closestWeakHeadPath = pathfinding.getShortestPath(weakHeads);
+
+    if (closestWeakHeadPath) {
+      const nextPosition = pathfinding.getNextPosition(closestWeakHeadPath);
+      const movesAvailable = directionManager.getSafeMoves(nextPosition).length !== 0;
+      const noAdjacentKiller = directionManager.noAdjacentKillerHead(nextPosition);
+
+      if (movesAvailable && noAdjacentKiller) {
+        const move = directionManager.directionTo(nextPosition);
+        debug('kill');
+        debug(move);
+        return move;
+      }
+    }
+  }
+
+  // FOOD!
+  const strongSnakes = board.snakes.filter((snake) => snake.body.length >= you.body.length);
+  const strongSnakesExist = strongSnakes.length !== 0;
+
+  if (foodExists && ((strongSnakesExist && smallEnough) || hungry)) {
+    const closestFoodPath = pathfinding.getShortestPath(board.food);
+
+    if (closestFoodPath) {
+      const nextPosition = pathfinding.getNextPosition(closestFoodPath);
+      const movesAvailable = directionManager.getSafeMoves(nextPosition).length !== 0;
+      const noAdjacentKiller = directionManager.noAdjacentKillerHead(nextPosition);
+
+      // debug('nextPosition', nextPosition);
+      // debug(directionManager.getSafeMoves(nextPosition));
+
+      if (movesAvailable && noAdjacentKiller) {
+        const move = directionManager.directionTo(nextPosition);
+        debug('food');
+        debug(move);
+        return move;
+      }
+    }
+  }
+
+  // DEFEND!
+  const tail = you.body[you.body.length - 1];
+  const tailTargets = directionManager.getSafeMoves(tail).map((move) => {
+    return directionManager.getPosition(tail, move);
+  });
+  const closestTailPath = pathfinding.getShortestPath([...tailTargets, tail]);
+
+  if (closestTailPath) {
+    const nextPosition = pathfinding.getNextPosition(closestTailPath);
+    const noAdjacentKiller = directionManager.noAdjacentKillerHead(nextPosition);
+
+    if (noAdjacentKiller) {
+      const move = directionManager.directionTo(nextPosition);
+      debug('defend');
+      debug(move);
+      return move;
+    }
+  }
+
+  // DO YOUR BEST!
+  const bestMoves = directionManager.getBestMoves();
+  let move = bestMoves[Math.floor(Math.random() * bestMoves.length)];
+
+  debug('random');
+  debug(move);
+
+  if (!move) {
+    move = 'up'; 
+    debug('no moves :(');
+  }
+
+  return move;
+};
+
+export { getMove };
